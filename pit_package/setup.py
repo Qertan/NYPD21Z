24ad8f22b6cdc6f424470d00e3528ca49c8fd213
Qertan <qertan.gaming@gmail.com>
2022-02-22 21:50:58 +0100

    version 1.0

--- pit_package/setup.py
+++ pit_package/setup.py
@@ -9,5 +9,5 @@ setup(
     author='Jakub Bandurski',
     author_email='j.bandurski@student.uw.edu.pl',
     description='Package calculates PIT statistics for Poland',
-    install_requires=["pandas", "numpy", "matplotlib", "openpyxl", "xlrd", "os"]
+    install_requires=["pandas", "numpy", "matplotlib", "openpyxl", "xlrd"]
 )
