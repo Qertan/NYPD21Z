4040ec74879e5ebc5b56baee46165a4951559e53
Qertan <qertan.gaming@gmail.com>
2022-02-22 16:15:00 +0100

    version 1.0

--- pit_package/setup.py
+++ pit_package/setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name='NYPD_zal',
     version='1.0',
-    packages=['pit_poland', 'pit_poland.analysis', 'pit_poland.data_export', 'pit_poland.data_import'],
-    url='',
+    packages=['pit_poland.analysis', 'pit_poland.data_import', 'pit_poland.data_export'],
+    url='https://github.com/Qertan/NYPD21Z',
     license='BSD 2-clause',
     author='Jakub Bandurski',
     author_email='j.bandurski@student.uw.edu.pl',
     description='Package calculates PIT statistics for Poland',
-    install_requires=["pandas", "matplotlib", "openpyxl", "xlrd"]
+    install_requires=["pandas", "numpy", "matplotlib", "openpyxl", "xlrd", "os"]
 )
